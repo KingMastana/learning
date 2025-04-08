5875de0a9319b1ee0420a54ad278703d1a394e37
KingMastana <40878648+KingMastana@users.noreply.github.com>
2025-04-08 00:37:51 +0000

    Use slots to pass data to the web component

--- web-components/userCard.js
+++ web-components/userCard.js
@@ -6,8 +6,16 @@ template.innerHTML = `
     }
 </style>
 <div class='user-card'>
-    <h3>
-    </h3>
+    <img />
+    <div>
+        <h3></h3>
+        <div class="info">
+            <p><slot name="email" /></p>
+            <p><slot name="phone"/></p>
+        </div>
+        <button id="toggle-info">Hide Info</button>
+    </div>
+    
 </div>
 `
 class UserCard extends HTMLElement {
@@ -16,6 +24,7 @@ class UserCard extends HTMLElement {
         this.attachShadow({mode: 'open'})
         this.shadowRoot.appendChild(template.content.cloneNode(true));
         this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name')
+        this.shadowRoot.querySelector('img').src = this.getAttribute('avatar')
     }
 }
 
